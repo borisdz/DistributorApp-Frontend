bfe1ee7f62fd7499496b5929956fe72d02b057b9
Boris Djotov <borisdzotov@gmail.com>
2025-05-04 11:51:04 +0200

    All functionalities for admin are coded.

--- src/app/admin/add-vehicle/add-vehicle.component.ts
+++ src/app/admin/add-vehicle/add-vehicle.component.ts
@@ -9,6 +9,8 @@ import {
 import { Router, RouterModule } from '@angular/router';
 import { AdminService } from '../../services/admin.service';
 import { NavbarComponent } from '../../navbar/navbar.component';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../../environment';
 
 @Component({
   selector: 'app-add-vehicle',
@@ -18,25 +20,50 @@ import { NavbarComponent } from '../../navbar/navbar.component';
 })
 export class AddVehicleComponent {
   form!: FormGroup;
+  warehouses: any[] = [];
 
   constructor(
     private fb: FormBuilder,
     private admin: AdminService,
-    private router: Router
+    private router: Router,
+    private http: HttpClient
   ) {}
 
   ngOnInit() {
     this.form = this.fb.group({
-      plateNumber: ['', Validators.required],
-      model: ['', Validators.required],
-      capacity: [null, Validators.required],
+      vehicleCarryWeight: ['', Validators.required],
+      vehicleServiceInterval: ['', Validators.required],
+      vehicleKilometers: ['', Validators.required],
+      vehicleLastService: ['', Validators.required],
+      vehicleLastServiceKm: ['', Validators.required],
+      vehiclePlate: ['', Validators.required],
+      vehicleVIN: ['', Validators.required],
+      vehicleRegDate: ['', Validators.required],
+      warehouseId: ['', Validators.required],
     });
+
+    this.loadWarehouses();
   }
 
   onSubmit() {
     if (this.form.invalid) return;
-    this.admin
-      .addVehicle(this.form.value)
-      .subscribe(() => this.router.navigate(['/admin/dashboard']));
+
+    const formData = new FormData();
+    Object.entries(this.form.value).forEach(([key, value]) => {
+      formData.append(key, value as any);
+    });
+
+    this.admin.addVehicle(formData).subscribe({
+      next: () => this.router.navigate(['/admin/dashboard']),
+      error: (e) => alert('Error adding vehicle: ' + e.message),
+    });
+  }
+
+  loadWarehouses() {
+    this.http
+      .get<any[]>(`${environment.apiUrl}/warehouse/all`)
+      .subscribe((data) => {
+        this.warehouses = data;
+      });
   }
 }
--- src/app/admin/add-warehouse/add-warehouse.component.ts
+++ src/app/admin/add-warehouse/add-warehouse.component.ts
@@ -9,6 +9,8 @@ import {
 import { Router, RouterModule } from '@angular/router';
 import { AdminService } from '../../services/admin.service';
 import { NavbarComponent } from '../../navbar/navbar.component';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../../environment';
 
 @Component({
   selector: 'app-add-warehouse',
@@ -18,25 +20,42 @@ import { NavbarComponent } from '../../navbar/navbar.component';
 })
 export class AddWarehouseComponent {
   form!: FormGroup;
+  cities: any[] = [];
 
   constructor(
     private fb: FormBuilder,
     private admin: AdminService,
-    private router: Router
+    private router: Router,
+    private http: HttpClient
   ) {}
 
   ngOnInit() {
     this.form = this.fb.group({
-      compName: ['', Validators.required],
       address: ['', Validators.required],
       city: [null, Validators.required],
     });
+
+    this.loadCities();
   }
 
   onSubmit() {
     if (this.form.invalid) return;
-    this.admin
-      .addWarehouse(this.form.value)
-      .subscribe(() => this.router.navigate(['/admin/dashboard']));
+
+    const formData = new FormData();
+
+    for (const [key, value] of Object.entries(this.form.value)) {
+      formData.append(key, value as any);
+    }
+
+    this.admin.addWarehouse(formData).subscribe({
+      next: () => this.router.navigate(['/admin/dashboard']),
+      error: (e) => alert('Error creating warehosue: ' + e.message),
+    });
+  }
+
+  loadCities() {
+    this.http
+      .get<any[]>(`${environment.apiUrl}/cities/listAll`)
+      .subscribe((data) => (this.cities = data));
   }
 }
