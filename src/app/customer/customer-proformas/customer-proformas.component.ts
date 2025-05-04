e1d4cf361088a6f7471aca8787a1ff8bee9f245c
Boris Djotov <borisdzotov@gmail.com>
2025-05-04 12:00:42 +0200

    Create base components needed for all customer functionalities

---
+++ src/app/customer/customer-proformas/customer-proformas.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-customer-proformas',
+  imports: [],
+  templateUrl: './customer-proformas.component.html',
+  styleUrl: './customer-proformas.component.css'
+})
+export class CustomerProformasComponent {
+
+}
